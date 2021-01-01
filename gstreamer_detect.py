4b605223e8d144d9d5c1eed25eb8f6a0e3364ad6
salmanfarsi100 <53941994+salmanfarsi100@users.noreply.github.com>
2021-01-01 17:46:42 +0500

    Optimal Frame Extractor
    Optimal frame extractor added.

--- gstreamer_detect.py
+++ gstreamer_detect.py
@@ -10,10 +10,13 @@ from gi.repository import GObject, Gst, GLib
 import sys
 import numpy
 import cv2
+from datetime import datetime
 
 vehicle_list = []		# vehicle bounding box metadata buffer
+rgb_frames_list = []    # image metadata buffer
+total_vehicles_detected = 0     # vehicles acccounted for in 'vehicle_list'
 
-# Vehicle Class ##########     # vehicle_list[] object class; described by the vehicle's tracking id, the number of frames it is tracked for and the coordinates of its bounding boxes
+########## Vehicle Class ##########     # vehicle_list[] object class; described by the vehicle's tracking id, the number of frames it is tracked for and the coordinates of its bounding boxes
 
 class Vehicle:
     def __init__(self, vehicle_id, frames_list=[], x1_list=[], y1_list=[], x2_list=[], y2_list=[]):
@@ -25,6 +28,15 @@ class Vehicle:
         self.y2_list = y2_list
 
 ########## Vehicle Class ##########
+
+########## RGB Frame Class ##########   # 'rgb_frames_list[]' object class; described by the frame number and the pixel matrix of the frame 
+
+class RGB_Frame:
+    def __init__(self, frame_iterator, rgb_image):
+        self.frame_iterator = frame_iterator
+        self.rgb_image = rgb_image
+
+########## RGB Frame Class ##########
 
 def IOU(x11, y11, x21, y21, x12, y12, x22, y22):		# intersection over union of two bounding boxes
     b1 = (x11, y11, x21, y21)
@@ -47,6 +59,17 @@ def check_overlap(x11, y11, x21, y21, x12, y12, x22, y22):		# is there an overla
         return False
     else:
         return True
+
+def vehicles_detected():
+    vehicle_count = 0
+    for v in vehicle_list:
+        if v.frames_list[-1] < (frame_number - 50):
+            vehicle_count += 1
+            print('\n vehicle id: ', v.vehicle_id, ' total frames = ', len(v.frames_list))
+            for i, l in enumerate(v.frames_list):
+                print('frame:', v.frames_list[i], 'x1:', v.x1_list[i], 'y1:',
+                      v.y1_list[i], 'x2:', v.x2_list[i], 'y2:', v.y2_list[i])
+    print(str(vehicle_count)+' vehicles detected')
 
 # Constructing Argument Parse to input from Command Line
 ap = argparse.ArgumentParser()
@@ -72,6 +95,7 @@ frame_number = 0
 tracking_id = 0
 y1 = 178
 y2 = 477
+midpoint = int((y1 + y2)/2)     # reference point of optimality
 
 global stop
 stop = 0
@@ -80,20 +104,13 @@ def bus_call(bus, message, loop):
     t = message.type
     if t == Gst.MessageType.EOS:
         sys.stdout.write("End of stream\n")
+        print(total_vehicles_detected, ' vehicles detected')
         print("[Info] Elapsed time: {:.2f}".format(fps.elapsed()))
         print("[Info] FPS:  {:.2f}".format(fps.fps()))
         print("[Info] Total frames: ", frame_number)
         
-        vehicle_count = 0
-        for v in vehicle_list:
-            if v.frames_list[-1] < (frame_number - 50):
-                vehicle_count += 1
-                print('\n vehicle id: ', v.vehicle_id)
-                for i, l in enumerate(v.frames_list):
-                    print('frame:', v.frames_list[i], 'x1:', v.x1_list[i], 'y1:',
-                          v.y1_list[i], 'x2:', v.x2_list[i], 'y2:', v.y2_list[i])
-        print(str(vehicle_count)+' vehicles detected')
-            
+        # vehicles_detected()
+        
         quit()
         loop.quit()
     elif t == Gst.MessageType.WARNING:
@@ -212,7 +229,12 @@ while True:
         # frame = imutils.resize(frame, width=1920)
         (h, w) = frame.shape[:2]
         # print(h, w)
-
+        
+        rgb_frames_list.append(RGB_Frame(frame_number, frame))    # save current image to 'rgb_frames_list'
+        if len(rgb_frames_list) > 250:      # drop expired frames; extend frame life by increasing this value (may affect performance)
+            for x in range(50):
+                del rgb_frames_list[x]
+        
         cv2.line(frame, (0, int(0.2 * h)),
                  (w, int(0.2 * h)), (0, 255, 0), thickness=2)
 
@@ -319,25 +341,72 @@ while True:
                         cv2.putText(frame, label, (startX, y),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)
 
-        cv2.imshow("frame", frame)
+        # cv2.imshow("frame", frame)
 
         cv2.imwrite("/home/ee/Caffe-SSD-Object-Detection/Object Detection Caffe/stream/frame_number=" +
-        str(frame_number)+".jpg", frame)
+        str(frame_number)+".jpg", frame)    # save current frame to disk
         
         frame_number += 1
 
         if stop == 1:
             break
         
-        # discard false detections (without sufficient number of frames)
         for i, x in enumerate(vehicle_list):
-            frame_lag = frame_number - x.frames_list[-1]
+            frame_lag = frame_number - x.frames_list[-1]    # how far behind the current frame is the last tracking frame of the vehicle
             l = len(x.frames_list)
-            if (frame_lag > 50 and l < 9):
-                print('vehicle '+str(vehicle_list[i].vehicle_id)+' deleted')
+            if (frame_lag > 50 and l < 9):      # eliminate false (without sufficient number of frames) tracking trains
+                print('insufficient frames, vehicle '+str(vehicle_list[i].vehicle_id)+' deleted')
                 del vehicle_list[i]
                 break
-
+            
+            if (frame_lag > 100 and frame_lag < 200):    # optimal frame extractor
+                total_vehicles_detected += 1
+                # print('midpoint', midpoint)
+                yc_list_temp = []   # temporary list of 'y' centres
+                xc_list_temp = []   # temporary list of 'x' centres
+                for j, c in enumerate(x.frames_list):   # populate a list of 'y' centres
+                    yc = int((x.y1_list[j] + x.y2_list[j]) / 2)
+                    yc_list_temp.append(yc)
+                    xc = int((x.x1_list[j] + x.x2_list[j]) / 2)
+                    xc_list_temp.append(xc)
+                # print('yc_list_temp', yc_list_temp)
+                # print('xc_list_temp', xc_list_temp)
+                distance_from_center = [d - midpoint for d in yc_list_temp]
+                # print('dis_from_c', distance_from_center)
+                pos = (np.abs(distance_from_center)).argmin()
+                # print('pos', pos)
+                temp_frame_number = x.frames_list[pos]
+                # print('temp_frame_number', temp_frame_number)
+                temp_id = x.vehicle_id
+                # print('temp_id', temp_id)
+                temp_x1 = int(x.x1_list[pos])
+                temp_x2 = int(x.x2_list[pos])
+                temp_y1 = int(x.y1_list[pos])
+                temp_y2 = int(x.y2_list[pos])
+                # print('crop coordinates', (temp_x1, temp_y1, temp_x2, temp_y2))
+                now = datetime.now()
+                dt_string = now.strftime('%d-%m-%Y_%H-%M-%S')
+                print('train expired, vehicle ' + str(vehicle_list[i].vehicle_id) + ' deleted')
+                del vehicle_list[i]     # discard the vehicle saved to disk
+                frame_finder = 0
+                for f in rgb_frames_list:   # find the optimal frame in 'rgb_frames_list'
+                    if f.frame_iterator == temp_frame_number:
+                        break
+                    else:
+                        frame_finder += 1
+                # print('frame_finder', frame_finder)
+                # print('frame shape', rgb_frames_list[frame_finder].rgb_image.shape)
+                crop = (rgb_frames_list[frame_finder].rgb_image)[temp_y1:temp_y2, temp_x1:temp_x2]      # crop the part of the frame bounding the vehicle
+                # print(crop.shape)
+                # print(dt_string)
+                cv2.imwrite("/home/ee/Caffe-SSD-Object-Detection/Object Detection Caffe/optimal_frame/trid_frno_time=" + str(temp_id) + '_' + str(temp_frame_number) + "_" + str(dt_string[11:19])+ ".jpg", crop)    # save current frame to disk
+                break
+            
+            if frame_lag > 200:     # 'vehicle_list' buffer cleaner; drops expired tracking trains
+                print('train expired, vehicle ' + str(vehicle_list[i].vehicle_id) + ' deleted', '\n')
+                del vehicle_list[i]
+                break
+        
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord('q'):
@@ -358,6 +427,7 @@ while True:
             elif message.type == Gst.MessageType.EOS:
                 stop = 1
                 print("End-Of-Stream reached.")
+                print(str(total_vehicles_detected) + ' vehicles detected')
                 fps.stop()
                 cv2.destroyAllWindows()
                 break
@@ -379,14 +449,6 @@ print("[Info] Elapsed time: {:.2f}".format(fps.elapsed()))
 print("[Info] FPS:  {:.2f}".format(fps.fps()))
 print("[Info] Total frames: ", frame_number)
 
-vehicle_count = 0
-for v in vehicle_list:
-    if v.frames_list[-1] < (frame_number - 50):
-        vehicle_count += 1
-        print('\n vehicle id: ', v.vehicle_id, ' total frames = ', len(v.frames_list))
-        for i, l in enumerate(v.frames_list):
-            print('frame:', v.frames_list[i], 'x1:', v.x1_list[i], 'y1:',
-                  v.y1_list[i], 'x2:', v.x2_list[i], 'y2:', v.y2_list[i])
-print(str(vehicle_count)+' vehicles detected')
+# vehicles_detected()
 
 pipeline.set_state(Gst.State.NULL)
